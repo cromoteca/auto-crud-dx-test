51a0ca751d3b97c49dfcaff59bfcb2165a77d865
Luciano Vernaschi <luciano@vaadin.com>
2023-11-08 10:17:39 +0100

    View skeletons

--- frontend/views/crud/PersonCrud.tsx
+++ frontend/views/crud/PersonCrud.tsx
@@ -1,21 +1,3 @@
-import { ExperimentalAutoCrud } from "@hilla/react-crud";
-import PersonModel from "Frontend/generated/com/example/application/entities/PersonModel";
-import { PersonService } from "Frontend/generated/endpoints";
-import { TextField } from "@hilla/react-components/TextField";
-
 export default function PersonCrud() {
-    return <ExperimentalAutoCrud model={PersonModel} service={PersonService} gridProps={
-        { visibleColumns: ["firstName", "lastName", "email", "phoneNumber", "birthDate", "occupation"] }
-    } formProps={{
-        customLayoutRenderer: {
-            template: [
-                [{ property: 'username', colSpan: 1 }, { property: 'password', colSpan: 1 }]
-            ]
-        },
-        fieldOptions:
-        {
-            'username': { renderer: ({ field }) => <TextField {...field} label="Username" readonly /> },
-            'password': { renderer: () => <></> }
-        }
-    }} />;
+    return <></>;
 }
--- frontend/views/form/PersonForm.tsx
+++ frontend/views/form/PersonForm.tsx
@@ -2,29 +2,6 @@ import { ExperimentalAutoForm } from "@hilla/react-crud";
 import PersonModel from "Frontend/generated/com/example/application/entities/PersonModel";
 import { PersonService } from "Frontend/generated/endpoints";
 import { PasswordField } from "@hilla/react-components/PasswordField";
-import { useEffect, useState } from "react";
-import Person from "Frontend/generated/com/example/application/entities/Person";
-import { TextField } from "@hilla/react-components/TextField";
-
 export default function PersonForm() {
-    const [person, setPerson] = useState<Person | undefined>();
-
-    useEffect(() => {
-        PersonService.get(5).then(setPerson)
-    }, []);
-
-    return <ExperimentalAutoForm
-        model={PersonModel}
-        service={PersonService}
-        item={person}
-        customLayoutRenderer={{
-            template: [
-                [{ property: 'username', colSpan: 1 }, { property: 'password', colSpan: 1 }]
-            ]
-        }}
-        fieldOptions={{
-            'username': { renderer: ({ field }) => <TextField {...field} label="Username" readonly /> },
-            'password': { renderer: () => <></> }
-        }}
-    />;
+    return <></>;
 }
