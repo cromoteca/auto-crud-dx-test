c8f6c6a2ab54267de84f705b1d9ba437b1707827
Luciano Vernaschi <luciano@vaadin.com>
2023-11-08 10:01:42 +0100

    Test bug on renderer

--- frontend/views/form/PersonForm.tsx
+++ frontend/views/form/PersonForm.tsx
@@ -1,22 +1,30 @@
 import { ExperimentalAutoForm } from "@hilla/react-crud";
 import PersonModel from "Frontend/generated/com/example/application/entities/PersonModel";
 import { PersonService } from "Frontend/generated/endpoints";
-import {PasswordField} from "@hilla/react-components/PasswordField";
-import {useEffect, useState} from "react";
+import { PasswordField } from "@hilla/react-components/PasswordField";
+import { useEffect, useState } from "react";
 import Person from "Frontend/generated/com/example/application/entities/Person";
+import { TextField } from "@hilla/react-components/TextField";
 
 export default function PersonForm() {
     const [person, setPerson] = useState<Person | undefined>();
 
-    useEffect(()=> {
+    useEffect(() => {
         PersonService.get(5).then(setPerson)
     }, []);
 
-    return <ExperimentalAutoForm model={PersonModel} service={PersonService} item={person} fieldOptions={
-        {
-            'password': {
-                renderer: ({ field }) => <PasswordField {...field} label="Password" />
-            }
-        }
-    } />;
+    return <ExperimentalAutoForm
+        model={PersonModel}
+        service={PersonService}
+        item={person}
+        customLayoutRenderer={{
+            template: [
+                [{ property: 'username', colSpan: 1 }, { property: 'password', colSpan: 1 }]
+            ]
+        }}
+        fieldOptions={{
+            'username': { renderer: ({ field }) => <TextField {...field} label="Username" readonly /> },
+            'password': { renderer: () => <></> }
+        }}
+    />;
 }
