da2490628b5087f66364c0aa2eb2568429bd22cf
Luciano Vernaschi <luciano@vaadin.com>
2023-11-09 16:29:46 +0100

    Remove imports

--- frontend/views/form/PersonForm.tsx
+++ frontend/views/form/PersonForm.tsx
@@ -1,7 +1,3 @@
-import { ExperimentalAutoForm } from "@hilla/react-crud";
-import PersonModel from "Frontend/generated/com/example/application/entities/PersonModel";
-import { PersonService } from "Frontend/generated/endpoints";
-import { PasswordField } from "@hilla/react-components/PasswordField";
 export default function PersonForm() {
     return <></>;
 }
